696899bf096c74aef536867429bdb803e9513c4c
arina <awwwrina@gmail.com>
2022-05-20 23:56:54 +0300

    contact us page(validation)

--- src/components/mini-card-item/mini-card-item.js
+++ src/components/mini-card-item/mini-card-item.js
@@ -8,7 +8,7 @@ const Card = ({img, name, country, price}) => {
     return (
         <li className="wrapper">
             <div className="item">
-            <img src={img} alt={name} />
+            <img src={img} alt={name}/>
             <div className="item_title">{slicedName}</div>
             <div className="item_country">{country}</div>
             <div className="item_price">{price}$</div>
--- src/components/pages/contact-us/contact-us.js
+++ src/components/pages/contact-us/contact-us.js
@@ -1,6 +1,11 @@
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from 'yup';
+
 import Navigation from "../../navigation/navigation";
 import BlackBeans from "../../black-beans/black-beans";
 import CoffeeNotes from '../../../image/coffeeNotes.jpg';
+import Footer from '../../footer/footer';
+
 import './contact-us.scss';
 const ContactUs = () => {
     return(
@@ -12,34 +17,100 @@ const ContactUs = () => {
            
             <section className="contacts">
 
-                    <h2 className="title">Contact our team</h2>
-                    <BlackBeans />
-                    <div className="container">
-                        
-                        <img src={CoffeeNotes} alt="CoffeeNotes" className="contacts__img"/>
-                        
-                        <p className="contacts__request">
-                            You're not going to hit a ridiculously long phone menu when you call us. Your email isn't going to the inbox abyss, never to be seen or heard from again. We provide the exceptional service we'd want to experience ourselves!
-                        </p>
-                        <form action="#" className="contacts__form">
-                            <div required class="contacts__input contacts__input_name">
-                                <input name="name" id="name" type="text"/>
-                                <label for="name">Name</label>
-                            </div>
-                            <div required class="contacts__input contacts__input_email">
-                                <input name="email" id="email" type="email"/>
-                                <label for="email">Email</label>
-                            </div>
-                            <div class="contacts__textarea">
-                                <textarea name="text" id="text"></textarea>
-                                <label for="text">Message</label>
-                            </div>
-                            <button class="contacts__btn btn">Send</button>
-                        </form>
-                    </div>
+                <h2 className="title">Contact our team</h2>
+                <BlackBeans />
+                <div className="container">
                     
+                    <img src={CoffeeNotes} alt="CoffeeNotes" className="contacts__img"/>
+                    
+                    <p className="contacts__request">
+                        Have a question? Concern? Request? We’d love to hear from you. Connect with us through the following ways.
+                        <br /> <br />
+                        You're not going to hit a ridiculously long phone menu when you call us. Your email isn't going to the inbox abyss, never to be seen or heard from again. We provide the exceptional service we'd want to experience ourselves!
+                    </p>
+                    <Formik
+                        initialValues = {{
+                            name:'', 
+                            email:'',
+                            text:''}}
+                        validationSchema = {Yup.object({
+                            name: Yup.string()
+                                    .required('Required field')
+                                    .min(2, 'Must be longer than 2 characters'),
+                            email: Yup.string()
+                                    .required('Required field')
+                                    .email('Incorrect email address'),
+                            text: Yup.string()
+                                    .required('Required field')
+                        })}
+                        onSubmit = {values => console.log(JSON.stringify(values, null, 2))}>
+                    
+                        <Form className="contacts__form" >
+                            <div required className="contacts__input contacts__input_name">
+                                <Field 
+                                    name="name" 
+                                    type="text"
+                                    placeholder="Name"
+                                    autoComplete="off"/>
+                            </div>
+                            <ErrorMessage 
+                                name="name" 
+                                className="contacts__error contacts__error_name"
+                                component="div"/>
+                            <div required className="contacts__input contacts__input_email">
+                                <Field
+                                    name="email" 
+                                    type="email"
+                                    placeholder="Email"
+                                    autoComplete="off"/>
+                            </div>
+                            <ErrorMessage 
+                                name="email" 
+                                className="contacts__error contacts__error_email"
+                                component="div"/>
+                            <div className="contacts__textarea">
+                                <Field 
+                                    name="text" 
+                                    id="text" 
+                                    placeholder="Message"
+                                    as="textarea"/>
+                            </div>
+                            <ErrorMessage 
+                                name="text"
+                                className="contacts__error contacts__error_textarea" 
+                                component="div"/>
+                            <button className="contacts__btn btn">Send</button>
+                        </Form>
+                    </Formik>
+                </div>
 
-             </section>
+                <div className="wrapper">
+                    <div className="hours">
+                        <h2 className="hours__header">Business hours:</h2>
+                        <p className="hours__interval">8 a.m. to 4 p.m., Monday-Friday</p>
+                    </div>
+                    <div className="phone">
+                        <h2 className="phone__header">Phone:</h2>
+                        <a href="tel:+18475555555">1-847-555-5555</a>
+                    </div>
+                    <div className="email">
+                        <h2 className="email__header">Email:</h2>
+                        <a href="mailto:contactus@aromacafe.com">contactus@aromacafe.com</a>
+                    </div>
+                    <div className="address">
+                        <h2 className="address__header">Address:</h2>
+                        <address>
+                            70 E Central St Suite 101B, 
+                            <br />
+                            Franklin, MA 02038
+                        </address>
+                    </div>
+                </div>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2961.200762194992!2d-71.39638178415312!3d42.08174787920661!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e46f8d899cb041%3A0x50516d0c2f6339e7!2zNzAgRSBDZW50cmFsIFN0ICMxMDFiLCBGcmFua2xpbiwgTUEgMDIwMzgsINCh0KjQkA!5e0!3m2!1sru!2sru!4v1652874294630!5m2!1sru!2sru" width="100%" height="450"></iframe>
+                
+
+            </section>
+            <Footer/>
         </>
     )
 
