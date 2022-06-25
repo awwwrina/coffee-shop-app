46983d51acf46b4ae4b86f8f6023be90822ca919
arina <awwwrina@gmail.com>
2022-06-25 03:33:11 +0300

    config

--- src/config.js
+++ src/config.js
@@ -4,5 +4,3 @@ export const DEV_SERVER_PORT = 3000;
 export const PROD_SERVER_PORT = 80;
 
 export const URL = process.env.NODE_ENV === 'production' ? `${PROD_SERVER_ADDRESS}:${PROD_SERVER_PORT}` : `${DEV_SERVER_ADDRESS}:${DEV_SERVER_PORT}`;
-export const SERVER_ADDRESS = 'localhost';
-export const SERVER_PORT = 80;
